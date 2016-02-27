74717dce399dfc55a9889e85091b38d43d18347a
Claire <claire.mouton@gmail.com>
2016-02-27 21:39:10 +0100

    Added watch on html!

--- gulp/tasks/tasks.js
+++ gulp/tasks/tasks.js
@@ -7,12 +7,14 @@ gulp.task('dev', () => {
 	run('clean:dev', ['html:dev', 'js:dev', 'css:dev', 'data:dev'], 'server:dev', 'test:dev');
 	gulp.watch(config.path.css.files, ['css:dev']);
 	gulp.watch(config.path.js.files, ['js:dev']);
+	gulp.watch(config.path.html.files, ['html:dev']);
 });
 
 gulp.task('dist', () => {
 	run('clean:dist', ['html:dist', 'js:dist', 'css:dist', 'data:dist'], 'server:dist');	
 	gulp.watch(config.path.css.files, ['css:dist']);
 	gulp.watch(config.path.js.files, ['js:dist']);
+	gulp.watch(config.path.html.files, ['html:dist']);
 });
 
 gulp.task('default', ['dev']);
