0875818843f0b4f3282b2027d694eb934ca1afec
Claire <claire.mouton@gmail.com>
2016-02-29 09:22:43 +0100

    Improved error handling when browserify is used in gulp context

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -1,9 +1,9 @@
 import gulp from 'gulp';
-import merge from 'merge-stream'
-import browserify from 'browserify'
+import merge from 'merge-stream';
+import browserify from 'browserify';
 import source from 'vinyl-source-stream';
-import buffer from 'vinyl-buffer'
-import uglify from 'gulp-uglify'
+import buffer from 'vinyl-buffer';
+import uglify from 'gulp-uglify';
 
 import config from '../config';
 
@@ -19,9 +19,12 @@ gulp.task('js:dev', () => {
   })
   .transform("babelify", {presets: ["es2015", "react"]})
   .bundle()
+  .on('error', function (err) {
+    config.handleError(err);
+    this.emit('end');
+  })
   .pipe(source('bundle.js')) //bundle js name
-  .pipe(gulp.dest(config.dev.js))
-  .on('error', config.handleError);
+  .pipe(gulp.dest(config.dev.js));
   return merge(thirdjs, js);
 });
 
@@ -37,11 +40,14 @@ gulp.task('js:dist', () => {
   })
   .transform("envify", {global:true, NODE_ENV: "production"}, {global: true})
   .transform("babelify", {presets: ["es2015", "react"]})
-  .bundle()
+  .bundle()  
+  .on('error', function (err) {
+    config.handleError(err);
+    this.emit('end');
+  })
   .pipe(source('bundle.js')) //bundle js name
   .pipe(buffer()) // <----- convert from streaming to buffered vinyl file object
   .pipe(uglify()) // now gulp-uglify works 
-  .pipe(gulp.dest(config.dist.js))
-  .on('error', config.handleError);
+  .pipe(gulp.dest(config.dist.js));
   return merge(thirdjs, js);
 });
